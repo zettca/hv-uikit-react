561007f12f4c8574a7a3f199730f1a677c305bb2
Bruno Henriques <zettca@users.noreply.github.com>
2024-05-22 12:34:37 +0100

    feat: add new palette colors
    refactor: update pentaho+ to use new palette

---
+++ packages/styles/src/tokens/colorsPalette.ts
@@ -0,0 +1,246 @@
+export const slate = {
+  slate_50: "#F8FAFC",
+  slate_100: "#F1F5F9",
+  slate_200: "#E2E8F0",
+  slate_300: "#CBD5E1",
+  slate_400: "#94A3B8",
+  slate_500: "#64748B",
+  slate_600: "#475569",
+  slate_700: "#334155",
+  slate_800: "#1E293B",
+  slate_900: "#0F172A",
+  slate_950: "#020617",
+} as const;
+
+export const neutral = {
+  neutral_50: "#FAFAFA",
+  neutral_100: "#F5F5F5",
+  neutral_200: "#E5E5E5",
+  neutral_300: "#D4D4D4",
+  neutral_400: "#A3A3A3",
+  neutral_500: "#737373",
+  neutral_600: "#525252",
+  neutral_700: "#404040",
+  neutral_800: "#262626",
+  neutral_900: "#171717",
+  neutral_950: "#0A0A0A",
+} as const;
+
+export const blue = {
+  blue_50: "#EFF6FF",
+  blue_100: "#DBEAFE",
+  blue_200: "#BFDBFE",
+  blue_300: "#93C5FD",
+  blue_400: "#60A5FA",
+  blue_500: "#3B82F6",
+  blue_600: "#2563EB",
+  blue_700: "#1D4ED8",
+  blue_800: "#1E40AF",
+  blue_900: "#1E3A8A",
+  blue_950: "#172554",
+} as const;
+
+export const red = {
+  red_50: "#FEF2F2",
+  red_100: "#FEE2E2",
+  red_200: "#FECACA",
+  red_300: "#FCA5A5",
+  red_400: "#F87171",
+  red_500: "#EF4444",
+  red_600: "#DC2626",
+  red_700: "#B91C1C",
+  red_800: "#991B1B",
+  red_900: "#7F1D1D",
+  red_950: "#450A0A",
+} as const;
+
+export const orange = {
+  orange_50: "#FFF7ED",
+  orange_100: "#FFEDD5",
+  orange_200: "#FED7AA",
+  orange_300: "#FDBA74",
+  orange_400: "#FB923C",
+  orange_500: "#F97316",
+  orange_600: "#EA580C",
+  orange_700: "#C2410C",
+  orange_800: "#9A3412",
+  orange_900: "#7C2D12",
+  orange_950: "#431407",
+} as const;
+
+export const yellow = {
+  yellow_50: "#FEFCE8",
+  yellow_100: "#FEF9C3",
+  yellow_200: "#FEF08A",
+  yellow_300: "#FDE047",
+  yellow_400: "#FACC15",
+  yellow_500: "#EAB308",
+  yellow_600: "#CA8A04",
+  yellow_700: "#A16207",
+  yellow_800: "#854D0E",
+  yellow_900: "#713F12",
+  yellow_950: "#422006",
+} as const;
+
+export const lime = {
+  lime_50: "#F7FEE7",
+  lime_100: "#ECFCCB",
+  lime_200: "#D9F99D",
+  lime_300: "#BEF264",
+  lime_400: "#A3E635",
+  lime_500: "#84CC16",
+  lime_600: "#65A30D",
+  lime_700: "#4D7C0F",
+  lime_800: "#3F6212",
+  lime_900: "#365314",
+  lime_950: "#1A2E05",
+} as const;
+
+export const green = {
+  green_50: "#F0FDF4",
+  green_100: "#DCFCE7",
+  green_200: "#BBF7D0",
+  green_300: "#86EFAC",
+  green_400: "#4ADE80",
+  green_500: "#22C55E",
+  green_600: "#16A34A",
+  green_700: "#15803D",
+  green_800: "#166534",
+  green_900: "#14532D",
+  green_950: "#052E16",
+} as const;
+
+export const teal = {
+  teal_50: "#F0FDFA",
+  teal_100: "#CCFBF1",
+  teal_200: "#99F6E4",
+  teal_300: "#5EEAD4",
+  teal_400: "#2DD4BF",
+  teal_500: "#14B8A6",
+  teal_600: "#0D9488",
+  teal_700: "#0F766E",
+  teal_800: "#115E59",
+  teal_900: "#134E4A",
+  teal_950: "#042F2E",
+} as const;
+
+export const cyan = {
+  cyan_50: "#ECFEFF",
+  cyan_100: "#CFFAFE",
+  cyan_200: "#A5F3FC",
+  cyan_300: "#67E8F9",
+  cyan_400: "#22D3EE",
+  cyan_500: "#06B6D4",
+  cyan_600: "#0891B2",
+  cyan_700: "#0E7490",
+  cyan_800: "#155E75",
+  cyan_900: "#164E63",
+  cyan_950: "#083344",
+} as const;
+
+export const sky = {
+  sky_50: "#F0F9FF",
+  sky_100: "#E0F2FE",
+  sky_200: "#BAE6FD",
+  sky_300: "#7DD3FC",
+  sky_400: "#38BDF8",
+  sky_500: "#0EA5E9",
+  sky_600: "#0284C7",
+  sky_700: "#0369A1",
+  sky_800: "#075985",
+  sky_900: "#0C4A6E",
+  sky_950: "#082F49",
+} as const;
+
+export const indigo = {
+  indigo_50: "#EEF2FF",
+  indigo_100: "#E0E7FF",
+  indigo_200: "#C7D2FE",
+  indigo_300: "#A5B4FC",
+  indigo_400: "#818CF8",
+  indigo_500: "#6366F1",
+  indigo_600: "#4F46E5",
+  indigo_700: "#4338CA",
+  indigo_800: "#3730A3",
+  indigo_900: "#312E81",
+  indigo_950: "#1E1B4B",
+} as const;
+
+export const violet = {
+  violet_50: "#F5F3FF",
+  violet_100: "#EDE9FE",
+  violet_200: "#DDD6FE",
+  violet_300: "#C4B5FD",
+  violet_400: "#A78BFA",
+  violet_500: "#8B5CF6",
+  violet_600: "#7C3AED",
+  violet_700: "#6D28D9",
+  violet_800: "#5B21B6",
+  violet_900: "#4C1D95",
+  violet_950: "#2E1065",
+} as const;
+
+export const fuchsia = {
+  fuchsia_50: "#FDF4FF",
+  fuchsia_100: "#FAE8FF",
+  fuchsia_200: "#F5D0FE",
+  fuchsia_300: "#F0ABFC",
+  fuchsia_400: "#E879F9",
+  fuchsia_500: "#D946EF",
+  fuchsia_600: "#C026D3",
+  fuchsia_700: "#A21CAF",
+  fuchsia_800: "#86198F",
+  fuchsia_900: "#701A75",
+  fuchsia_950: "#4A044E",
+} as const;
+
+export const pink = {
+  pink_50: "#FDF2F8",
+  pink_100: "#FCE7F3",
+  pink_200: "#FBCFE8",
+  pink_300: "#F9A8D4",
+  pink_400: "#F472B6",
+  pink_500: "#EC4899",
+  pink_600: "#DB2777",
+  pink_700: "#BE185D",
+  pink_800: "#9D174D",
+  pink_900: "#831843",
+  pink_950: "#500724",
+} as const;
+
+export const rose = {
+  rose_50: "#FFF1F2",
+  rose_100: "#FFE4E6",
+  rose_200: "#FECDD3",
+  rose_300: "#FDA4AF",
+  rose_400: "#FB7185",
+  rose_500: "#F43F5E",
+  rose_600: "#E11D48",
+  rose_700: "#BE123C",
+  rose_800: "#9F1239",
+  rose_900: "#881337",
+  rose_950: "#4C0519",
+} as const;
+
+/**
+ * Base color palette tokens. Should rarely be used directly (eg. charts)
+ * @private WIP. Currently in used for Pentaho+ theme
+ */
+export default {
+  ...slate,
+  ...neutral,
+  ...blue,
+  ...red,
+  ...orange,
+  ...yellow,
+  ...lime,
+  ...green,
+  ...teal,
+  ...cyan,
+  ...sky,
+  ...indigo,
+  ...violet,
+  ...fuchsia,
+  ...pink,
+  ...rose,
+};
