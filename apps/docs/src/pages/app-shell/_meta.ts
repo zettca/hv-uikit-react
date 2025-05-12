9871b20479b50402c97a3389891b042ef87bee3d
Bruno Henriques <zettca@users.noreply.github.com>
2025-05-12 18:09:20 +0100

    docs(AppShell): copy from app-shell repo

---
+++ apps/docs/src/pages/app-shell/_meta.ts
@@ -0,0 +1,30 @@
+export default {
+  "-- Concepts": {
+    title: "Concepts",
+    type: "separator",
+  },
+  introduction: "Introduction",
+  "base-concepts": "Base Concepts",
+  "config-file": "App Shell Configuration",
+  // #region configuration
+  "other-apps": "Registering other Application Bundles",
+  views: "Views and Routes",
+  navigation: "Navigation",
+  "header-actions": "Header Actions",
+  state: "Sharing State",
+  localization: "Localization",
+  theming: "Theming",
+  // #endregion
+  notifications: "Notifications",
+  styling: "Styling",
+  "assets-and-apis": "Static Assets and API Calls",
+  "dev-guidelines": "Development Guidelines",
+
+  // "manual-setup": "Manual Setup",
+
+  "-- API Reference": {
+    title: "API Reference",
+    type: "separator",
+  },
+  "api-reference": "API Reference",
+};
